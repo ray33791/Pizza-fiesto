843c329cbbb84fa33c07c642a40792b02e83f34f
ray33791 <101357637+ray33791@users.noreply.github.com>
2022-10-11 17:02:57 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.promo__carousel').slick({
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        prevArrow: '<button type="button" class="slick-prev"><img class="left_arrow" src="../img/slider/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img class="right_arrow" src="../img/slider/arrow_right.png"></button>',
+    });
+  });
